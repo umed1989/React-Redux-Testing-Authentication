77dceaa10a770b121e4452e83e226ed0213d2fc0
umed tolibi <tolibiumed@gmail.com>
2018-08-14 12:53:24 -0400

    Setting up Redux

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -10,7 +10,7 @@ class CommentBox extends Component {
 
   onFormSubmit = e => {
     e.preventDefault();
-
+    this.props.saveComment(this.state.comment);
     this.setState({ comment: "" });
   };
   render() {
