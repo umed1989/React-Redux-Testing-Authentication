64782efe471fd54d17e3e1128217259a3cccd1f1
umed tolibi <tolibiumed@gmail.com>
2018-08-19 22:00:31 -0400

    Setting up State Validation with tv4

--- src/Root.js
+++ src/Root.js
@@ -1,10 +1,15 @@
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import reduxPromise from "redux-promise";
+import async from "middlewares/async";
+import stateValidator from "middlewares/stateValidator";
 import React from "react";
 import reducers from "reducers";
 
 export default ({ children, initialState = {} }) => {
-  const store = createStore(reducers, initialState, applyMiddleware());
+  const store = createStore(
+    reducers,
+    initialState,
+    applyMiddleware(async, stateValidator)
+  );
   return <Provider store={store}>{children}</Provider>;
 };
