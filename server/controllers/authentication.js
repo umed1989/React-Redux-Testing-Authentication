3231af8c6d01c0009b33afd7a174692623ca2db6
umed tolibi <tolibiumed@gmail.com>
2018-08-25 18:10:15 -0400

    Pushing before encryption

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,26 @@
+const User = require("../models/user");
 exports.signup = (req, res, next) => {
-  res.send({ success: "true" });
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email: email }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (user) {
+      return res.status(422).send({ error: "Email is in use" });
+    }
+
+    const newUser = new User({
+      password: password,
+      email: email
+    });
+
+    newUser.save(function(err) {
+      if (err) {
+        return next(err);
+      }
+      res.json({ success: true });
+    });
+  });
 };
--- server/router.js
+++ server/router.js
@@ -1,6 +1,3 @@
 const Authentication = require("./controllers/authentication");
-module.exports = app => {
-  app.get("/", (req, res, next) =>
-    res.send(["your", "get", "request", "is working"])
-  );
-};
+
+module.exports = app => app.post("/signup", Authentication.signup);
