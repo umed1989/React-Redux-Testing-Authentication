a89db818eb04689ebd28b71485782175d52e9b6b
umed tolibi <tolibiumed@gmail.com>
2018-08-26 20:12:14 -0400

    Setting up bcrypt

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -3,6 +3,12 @@ exports.signup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
 
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: "You must provide email and password" });
+  }
+
   User.findOne({ email: email }, (err, user) => {
     if (err) {
       return next(err);
