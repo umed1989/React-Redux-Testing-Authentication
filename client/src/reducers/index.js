d143d644ef563a8b5954b94467654b673a53019f
umed tolibi <tolibiumed@gmail.com>
2018-09-11 17:49:32 -0400

    Setting up Redux-Form

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux";
 import auth from "./auth";
+import { reducer as formReducer } from "redux-form";
 export default combineReducers({
-  auth
+  auth,
+  form: formReducer
 });
